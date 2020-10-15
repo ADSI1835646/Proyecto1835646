bad2e5355b723aa2b13d3d7c6dd951f22e3e2874
sebasrmusik <72873947+sebasrmusik@users.noreply.github.com>
2020-10-15 09:05:38 -0400

    conex

--- conex/inserDatosFacturaVenta.php
+++ conex/inserDatosFacturaVenta.php
@@ -1,5 +1,14 @@
 <?php
   $conexion = mysqli_connect('localhost', 'root', '', 'sifprueba');
   if (isset($_POST["crearFactura"])) {
-    
+    $idFactura = $_POST["idConsecutivo"];
+    $fechaFactura = $_POST["fechaCrearFactura"];
+    $cedulaCliente = $_POST["cedulaCrearFactura"];
+    $nombreCliente = $_POST["clienteCrearFactura"];
+
+    //Tomar datos de productos
+    var_dump($_POST["articulo"]);
+
+    // $articulo = $_POST["articulo"];
+    // var_dump($articulo);
   }
--- form factura de venta.php
+++ form factura de venta.php
@@ -2,10 +2,10 @@
 <html lang="es">
 <?php
   $conexion = mysqli_connect('localhost', 'root', '', 'sifprueba');
+  include "funciones/func.php";
   
 ?>
-
-  <head>
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Factura de venta</title>
